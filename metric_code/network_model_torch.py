9c9ca680c4514156e8d8cbb97bd50d9e1f40faae
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-16 14:27:48 -0500

    .

--- metric_code/network_model_torch.py
+++ metric_code/network_model_torch.py
@@ -123,7 +123,7 @@ class JNDModel(nn.Module):
 
         others, loss_sum = self.feature_loss(ref, inp)
         others = torch.stack(others)
-        #print(f"others:{others.shape}")
+        print(f"others:{others.shape}")
         dist = self.sigmoid(others).reshape(-1, 1, 1)
 
         logits = self.classification_layer(dist).squeeze(1)
