ca2acde671635f9f5b1fbfb8253f2c652e796b8a
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-16 12:47:22 -0500

    .

--- metric_code/dataset_torch.py
+++ metric_code/dataset_torch.py
@@ -91,7 +91,7 @@ class JNDDataset(Dataset):
 
         print(f"label:{self.paths['labels'][idx]}")
 
-        label = torch.tensor(self.paths['labels'][idx], dtype=torch.LongTensor)
+        label = torch.tensor(self.paths['labels'][idx])
         return inp, out, label
 
 def collate_fn(batch):
