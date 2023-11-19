0e47b28874e86c79f4bc7721b18ef8e3721bbab5
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-19 16:07:47 -0500

    .

--- metric_code/network_model_torch.py
+++ metric_code/network_model_torch.py
@@ -114,11 +114,12 @@ class FeatureLossBatch(nn.Module):
         super().__init__()
         self.out_channels = [base_channels * (2 ** (i // 5)) for i in range(n_layers)]
         self.out_dims = []
+        out_dim = 20000
         for i in range(n_layers):
             if out_dim % 2 == 0: 
-                out_dim = 20000 // (2 ** i)
+                out_dim = out_dim // (2 ** i)
             else:
-                out_dim = 20000 // (2 ** i) + 1
+                out_dim = out_dim // (2 ** i) + 1
             self.out_dims.append(out_dim)
 
         if weights:
