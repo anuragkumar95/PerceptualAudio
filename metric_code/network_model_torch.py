8e0fa36c9d74b8eec8698e55025745fb90134fc1
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-19 16:28:22 -0500

    .

--- metric_code/network_model_torch.py
+++ metric_code/network_model_torch.py
@@ -150,7 +150,13 @@ class FeatureLossBatch(nn.Module):
 class JNDModel(nn.Module):
     def __init__(self, in_channels, n_layers, keep_prob, norm_type='sbn', gpu_id=None):
         super().__init__()
-        self.loss_net = LossNet(in_channels=in_channels, 
+        self.loss_net_inp = LossNet(in_channels=in_channels, 
+                                n_layers=n_layers, 
+                                kernel_size=3, 
+                                keep_prob=keep_prob, 
+                                norm_type=norm_type)
+        
+        self.loss_net_out = LossNet(in_channels=in_channels, 
                                 n_layers=n_layers, 
                                 kernel_size=3, 
                                 keep_prob=keep_prob, 
@@ -165,8 +171,8 @@ class JNDModel(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, inp, ref):
-        ref = self.loss_net(ref)
-        inp = self.loss_net(inp)
+        ref = self.loss_net_out(ref)
+        inp = self.loss_net_inp(inp)
 
         others = self.feature_loss(ref, inp)
         dist = torch.stack(others).mean(0)
