0e894e0e0d01627561ddca7bfd7c77df0d7c6fd2
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-19 15:58:35 -0500

    .

--- metric_code/network_model_torch.py
+++ metric_code/network_model_torch.py
@@ -129,6 +129,7 @@ class FeatureLossBatch(nn.Module):
         for i, (e1, e2) in enumerate(zip(embeds1, embeds2)):
             dist = e1 - e2
             dist = dist.permute(0, 1, 3, 2)
+            print(f"dist:{dist.shape}, weight:{self.weights[i].shape}")
             if self.weights is not None:
                 res = self.weights[i] * dist
             else:
